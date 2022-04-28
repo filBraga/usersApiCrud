40d116f4d3ffecc8e5bd16768aeff2312094312b
Filipe <filipe.b.braga@gmail.com>
2022-04-28 14:28:13 -0300

    up to 3 lint

--- Middlewares/loginMiddleware.js
+++ Middlewares/loginMiddleware.js
@@ -3,7 +3,7 @@ function generateToken(length) {
   // https://stackoverflow.com/questions/8532406/create-a-random-token-in-javascript-based-on-user-details
   const a = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split('');
   const b = [];  
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < length; i += 1) {
       const j = (Math.random() * (a.length - 1)).toFixed(0);
       b[i] = a[j];
   }
